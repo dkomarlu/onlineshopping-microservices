ece8aa585b2a44e0b822d906d347977f460624e6
Dharani <dkomarlu@yahoo.com>
2024-01-26 12:28:37 -0500

    Configured for CircuiteBraker and Resiliance4J

--- order-service/src/main/java/com/einfonet/programmingtechie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/einfonet/programmingtechie/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.einfonet.programmingtechie.orderservice.controller;
 
 import com.einfonet.programmingtechie.orderservice.dto.OrderRequest;
 import com.einfonet.programmingtechie.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,13 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
 
         return orderService.placeOrder(orderRequest);
+    }
+
+    public String fallbackMethod (OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Ooops Something went wrong,  try again later..";
     }
 }
